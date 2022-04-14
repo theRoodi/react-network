02766d126e25ec4a189ec0c4db6d5adba1ad3b39
roodi <igorrud95@gmail.com>
2022-04-14 23:18:26 +0700

    component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import './App.css';
 
 const App = () => {
 	return (
-		<div className="App">
-			under constraction
+		<div>
+			<Header />
+			<Technologies />
 		</div>
 	);
+}
+
+
+const Header = () => {
+	return(
+		<div>
+			<a href='#'>1</a>
+			<a href='#'>2</a>
+			<a href='#'>3</a>
+			<a href='#'>4</a>
+		</div>
+	)
+}
+const Technologies = () => {
+	return(
+		<div>
+			<div>One</div>
+			<div>Two</div>
+			<div>Three</div>
+		</div>
+	)
 }
 
 export default App;
