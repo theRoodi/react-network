eafbfd27ab3a179cbd99e00035c9160630591eaf
roodi <igorrud95@gmail.com>
2022-04-21 23:18:50 +0700

    14 css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import style from './Header.module.css'
+
 const Header = () => {
     return (
-        <div className='header'>
+        <div className={style.header}>
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/The_social_network.svg/2560px-The_social_network.svg.png' />
         </div>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,12 @@
+import style from './Profile.module.css'
+
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={style.content}>
 				<div>
 					<img src='https://images.ctfassets.net/hrltx12pl8hq/5KiKmVEsCQPMNrbOE6w0Ot/341c573752bf35cb969e21fcd279d3f9/hero-img_copy.jpg' />
 				</div>
-				<div className='avatar'>
+				<div className={style.avatar}>
 					<img src='https://static.vecteezy.com/system/resources/previews/000/425/632/large_2x/avatar-icon-vector-illustration.jpg' />
 				</div>
 
