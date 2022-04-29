c1cd9e607022ea3ff0f29d0e4c408eab7101b00d
roodi <igorrud95@gmail.com>
2022-04-29 21:28:43 +0700

    21 dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,31 @@
+import { NavLink } from 'react-router-dom'
 import style from './Dialogs.module.css'
 
 const Dialogs = (props) => {
+    const setActive = ({ isActive }) =>(isActive ? style.active: style.dialog);
+
     return (
-        <div>
-            <a>Dialogs</a>
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog}>
+                    <NavLink className={setActive} to='/1'>One</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink className={setActive} to='/2'>Two</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink className={setActive} to='/3'>Three</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink className={setActive} to='/4'>Four</NavLink>
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>hi</div>
+                <div className={style.message}>hi2</div>
+                <div className={style.message}>hi3</div>
+            </div>
+
         </div>
     )
 }
