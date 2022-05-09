41b472364d20e9bad4dcdf6bc647f92e9c129106
roodi <igorrud95@gmail.com>
2022-05-09 21:31:41 +0700

    fix bug

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,6 @@ import style from './MyPosts.module.css'
 import React from "react"
 
 const MyPosts = (props) => {
-    
     let postsElement = props.state.postsData.map((data) => {
         return <Post likeCount = {data.likeCount} views = {data.views} message = {data.message} />
     })
--- src/redux/state.js
+++ src/redux/state.js
@@ -2,9 +2,9 @@ import {rerender} from './../render'
 let state = {
     profilePage: {
         postsData: [
-            { id: 1, text: 'Hi!', likeCount: 15, views: 93 },
-            { id: 2, text: 'Hi2!', likeCount: 11, views: 43 },
-            { id: 3, text: 'Hi3!', likeCount: 12, views: 92 },
+            { id: 1, message: 'Hi!', likeCount: 15, views: 93 },
+            { id: 2, message: 'Hi2!', likeCount: 11, views: 43 },
+            { id: 3, message: 'Hi3!', likeCount: 12, views: 92 },
         ]
     },
     dialogPage: {
@@ -26,8 +26,9 @@ let state = {
 export let addPost = (post) => {
     let newPost = {
         id: 5,
-        text: post,
-        likeCount : 0
+        message: post,
+        likeCount : 0,
+        views : 0
     }
 
     state.profilePage.postsData.push(newPost)
